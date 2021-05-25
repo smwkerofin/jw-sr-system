4d42c7b55b282140db7856c1addee92804809c12
Stephen Watson <stephen@kerofin.co.uk>
2021-05-25 11:00:03 +0100

    Get and report state of Coordination and Mobility spells.  Calculate number of action tokens.

--- roll20/track_command.js
+++ roll20/track_command.js
@@ -13,6 +13,19 @@ function message_handler(msg) {
   }
 }
 
+function haveMobilityCast(cid) {
+    return getAttrByName(cid, "mobilCast")=="on";
+}
+
+function haveCoordCast(cid) {
+    return getAttrByName(cid, "coordCast")=="on";
+}
+
+function haveSlowCast(cid) {
+  // Char sheet doesn't support this yet
+    return false;
+}
+
 function list_tokens(who) {
   say("list tokens");
 
@@ -23,6 +36,8 @@ function list_tokens(who) {
   let i=0;
   for(i=0; i<tokens.length; i++) {
     say(tokens[i].get('name'));
+    var cid=tokens[i].get('represents');
+    //say(cid);
   }
 
   say("list characters");
@@ -31,8 +46,24 @@ function list_tokens(who) {
     });
   for(i=0; i<chars.length; i++) {
     say(chars[i].get('name'));
+    //say("id="+chars[i].id);
     let dex=getAttrByName(chars[i].id, "curdex");
     say("dex="+dex);
+    if(haveMobilityCast(chars[i].id)) {
+      say("Mobility active");
+    }
+    if(haveCoordCast(chars[i].id)) {
+      say("Coordination active");
+    }
+
+    say("Tokens="+calc_tokens(dex, haveMobilityCast(chars[i].id),
+			      haveCoordCast(chars[i].id),
+			      haveSlowCast(chars[i].id)));
+  }
+  var npcs = findObjs({ type: 'character', controlledby: '' });
+  say("NPCs");
+  for(i=0; i<npcs.length; i++) {
+    say(npcs[i].get('name'));
   }
 }
 
@@ -52,6 +83,37 @@ function removeGM(who) {
     return who.replace(/\(GM\)/, '').trim();
 }
 
+function calc_tokens(dex, coordination, mobility, slow) {
+    let toks=1;
+    if(dex<=8) {
+        toks=1;
+    } else if(dex<=12) {
+        toks=2;
+    } else if(dex<=15) {
+        toks=3;
+    } else if(dex<=18) {
+        toks=4;
+    } else {
+        toks=5;
+    }
+
+    if(coordination) {
+        toks++;
+    }
+    if(mobility) {
+        toks++;
+    }
+    if(slow) {
+        toks--;
+    }
+
+    if(toks<1) {
+      toks=1;
+    }
+
+    return toks;
+}
+
 // Event handlers
 on("chat:message", msg => {
     try {
@@ -66,3 +128,8 @@ on("chat:message", msg => {
       log(err.stack);
     }
   });
+
+// Attributes on RQ:G sheet
+// curdex
+// mobilCast on/0
+// coordCast on/0
